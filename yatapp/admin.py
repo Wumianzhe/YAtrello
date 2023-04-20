0a9b1f7a8f3b30705907685cfac0fe9af9a70848
Pavel <pashafizmat1@gmail.com>
2023-04-20 17:13:38 +0300

    first attempt database

--- yatapp/admin.py
+++ yatapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Board, Section, Group, UserGroup, Task, Comment, Subtasks
 # Register your models here.
+
+admin.site.register(Board)
+admin.site.register(Section)
+admin.site.register(Group)
+admin.site.register(UserGroup)
+admin.site.register(Task)
+admin.site.register(Comment)
+admin.site.register(Subtasks)
--- yatapp/models.py
+++ yatapp/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.core.validators import MaxValueValidator,MaxLengthValidator
+from django.contrib.auth.models import User
+from django.utils import timezone
+# Create your models here. 
 
-# Create your models here.
+class Group(models.Model):
+    id = models.IntegerField(validators=[MaxValueValidator(999)], primary_key=True)
+    is_visible = models.BooleanField(default=False)
+    name = models.CharField(max_length=25)
+
+
+class Board(models.Model):
+    id = models.IntegerField(validators=[MaxValueValidator(999)], primary_key=True)
+    name = models.CharField(max_length=25)
+    admin_gid = models.ForeignKey(Group, on_delete=models.CASCADE, related_name='board_admins')
+    user_gid = models.ForeignKey(Group, on_delete=models.CASCADE, related_name='board_users')
+
+
+class Section(models.Model):
+    id = models.IntegerField(validators=[MaxValueValidator(999)],primary_key=True)
+    name = models.CharField(max_length=25)
+    board_id = models.ForeignKey(Board,on_delete=models.CASCADE)
+
+class UserGroup(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    group_id = models.ForeignKey(Group, on_delete=models.CASCADE)
+
+class Task(models.Model):
+    id = models.IntegerField(validators=[MaxValueValidator(999)], primary_key=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='users_task')
+    author_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='authors_task')
+    text = models.TextField(validators=[MaxLengthValidator(999)])
+    section_id = models.ForeignKey(Section, on_delete=models.CASCADE)
+    is_completed = models.BooleanField(default=False)
+    time_start = models.DateTimeField(default=timezone.now)
+    time_deadline = models.DateTimeField(null=True)
+
+class Subtasks(models.Model):
+    id = models.IntegerField(validators=[MaxValueValidator(999)], primary_key=True)
+    task_id = models.ForeignKey(Task, on_delete=models.CASCADE)
+    is_completed = models.BooleanField(default=False)
+
+class Comment(models.Model):
+    id = models.IntegerField(validators=[MaxValueValidator(999)], primary_key=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    task_id =  models.ForeignKey(Task, on_delete=models.CASCADE)
