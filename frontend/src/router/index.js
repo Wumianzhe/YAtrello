cef7630067e23912e2dbbd15f947c1f334443288
Alexandr Kopnov <a.kopnov24@gmail.com>
2023-04-26 18:07:43 +0300

    Main page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,17 +2,16 @@ import Board from "../pages/Board";
 import Log from "../pages/Log";
 import Reg from "../pages/Reg";
 import Profile from "../pages/Profile";
+import FullWidthGrid from "../pages/Main";
 
 export const privateRoutes = [
-    {path: '/board', component: Board, exact: true},
-    {path: '/log', component: Log, exact: true},
-    {path: '/reg', component: Reg, exact: true},
-    {path: '/profile', component: Profile, exact: true},
+  { path: '/board', component: Board, exact: true },
+  { path: '/log', component: Log, exact: true },
+  { path: '/reg', component: Reg, exact: true },
+  { path: '/profile', component: Profile, exact: true },
+  { path: '/', component: FullWidthGrid, exact: true }
 ]
 
-export const publicRoutes = [
-    {path: '/board', component: Board, exact: true},
-    {path: '/log', component: Log, exact: true},
-    {path: '/reg', component: Reg, exact: true},
-    {path: '/profile', component: Profile, exact: true},
+export const publicRoutes = [ // Not used right now
+  { path: '/', component: Log, exact: true },
 ]
