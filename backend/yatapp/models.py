1ba8d841f0c3563596f737e19bc382335e38cd83
Pavel <pashafizmat1@gmail.com>
2023-04-26 18:25:28 +0300

    id autogeneration

--- backend/yatapp/models.py
+++ backend/yatapp/models.py
@@ -5,20 +5,20 @@ from django.utils import timezone
 # Create your models here. 
 
 class Group(models.Model):
-    id = models.IntegerField(validators=[MaxValueValidator(999)], primary_key=True)
+    id = models.AutoField(primary_key=True)
     is_visible = models.BooleanField(default=False)
     name = models.CharField(max_length=25)
     
 
 class Board(models.Model):
-    id = models.IntegerField(validators=[MaxValueValidator(999)], primary_key=True)
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=25)
     admin_gid = models.ForeignKey(Group, on_delete=models.CASCADE, related_name='board_admins')
     user_gid = models.ForeignKey(Group, on_delete=models.CASCADE, related_name='board_users')
 
 
 class Section(models.Model):
-    id = models.IntegerField(validators=[MaxValueValidator(999)],primary_key=True)
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=25)
     board_id = models.ForeignKey(Board, on_delete=models.CASCADE)
 
@@ -27,7 +27,7 @@ class UserGroup(models.Model):
     group_id = models.ForeignKey(Group, on_delete=models.CASCADE)
 
 class Task(models.Model):
-    id = models.IntegerField(validators=[MaxValueValidator(999)], primary_key=True)
+    id = models.AutoField(primary_key=True)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='users_task')
     author_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='authors_task')
     text = models.TextField(validators=[MaxLengthValidator(999)])
@@ -37,11 +37,11 @@ class Task(models.Model):
     time_deadline = models.DateTimeField(null=True)
 
 class Subtask(models.Model):
-    id = models.IntegerField(validators=[MaxValueValidator(999)], primary_key=True)
+    id = models.AutoField(primary_key=True)
     task_id = models.ForeignKey(Task, on_delete=models.CASCADE)
     is_completed = models.BooleanField(default=False)
 
 class Comment(models.Model):
-    id = models.IntegerField(validators=[MaxValueValidator(999)], primary_key=True)
+    id = models.AutoField(primary_key=True)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     task_id =  models.ForeignKey(Task, on_delete=models.CASCADE)
