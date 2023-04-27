a429098225abe37201eddee6906c99125abc8c2c
Alexandr Kopnov <a.kopnov24@gmail.com>
2023-04-27 21:22:04 +0300

    router tutorial, finally ignore package-lock.json
    Partway through, currently able to render existing data. After
    completion, will begin working towards applying it to project.
    Following that, cleanup all tutorial files

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import ReactDOM from 'react-dom/client'
+import {RouterProvider,createBrowserRouter} from "react-router-dom";
+import Root,{ loader as rootLoader} from './pages/Root';
+import Contact from './pages/contact';
+import ErrorPage from './pages/error-page';
+import './styles/index.css'
+
+export const routes = [
+{
+    path:"/",
+    element: <Root />,
+    errorElement: <ErrorPage/>,
+    loader: rootLoader,
+    children: [
+        {
+            path: "contacts/:contactId",
+            element: <Contact />
+        }
+    ]
+},
+]
+
+const router = createBrowserRouter(routes);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <React.StrictMode>
+      <RouterProvider router={router}/>
+    </React.StrictMode>
 );
