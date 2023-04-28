dfe3269ef05ff719eeacb39435da32644462871a
Alexandr Kopnov <a.kopnov24@gmail.com>
2023-04-28 12:18:24 +0300

    done with tutorial, related changes now
    I should have done it in different project altogether, but that's how it went

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
 import {RouterProvider,createBrowserRouter} from "react-router-dom";
-import Root,{ loader as rootLoader} from './pages/Root';
-import Contact from './pages/contact';
+import Root,{ loader as rootLoader, action as rootAction} from './pages/Root';
+import Contact, {loader as contactLoader} from './pages/contact';
+import EditContact from './pages/edit';
 import ErrorPage from './pages/error-page';
 import './styles/index.css'
 
@@ -12,10 +13,17 @@ export const routes = [
     element: <Root />,
     errorElement: <ErrorPage/>,
     loader: rootLoader,
+    action: rootAction,
     children: [
         {
             path: "contacts/:contactId",
+            loader: contactLoader,
             element: <Contact />
+        },
+        {
+            path: "contacts/:contactId/edit",
+            element: <EditContact/>,
+            loader: contactLoader,
         }
     ]
 },
