19dd15adf2b47025dd340d97d77ad0dafbfb79a6
Alexandr Kopnov <a.kopnov24@gmail.com>
2023-05-08 20:11:42 +0300

    misc stuff, half addition of users to groups

--- backend/yatapp/views.py
+++ backend/yatapp/views.py
@@ -6,6 +6,8 @@ from yatproj.serializers import BoardSerializer, SectionSerializer, GroupSeriali
 from rest_framework.permissions import AllowAny
 from rest_framework.authtoken.models import Token
 from django.db.models import Q
+from django.http import HttpResponseBadRequest
+import json
 
 @api_view(['GET'])
 def uid_by_token(request):
@@ -52,13 +54,17 @@ class BoardViewSet(viewsets.ModelViewSet):
         boards = Board.objects.filter(Q(admin_gid__in = usergroups) | Q(user_gid__in = usergroups)).values('id')
         serializer = BoardSerializer(boards, many = True)
         return Response(serializer.data)
-
     @action(detail=False, url_path='by_name/(?P<name>.+)')
     def by_name(self, request, name = None):
         queryset = self.get_queryset().filter(name=name)
         if not queryset.exists():
             return Response({'error':'Object not found'}, status= status.HTTP_404_NOT_FOUND)    
         serializer = self.get_serializer(queryset, many = True)
+        return Response(serializer.data)
+    @action(detail=True, methods=["get"], url_path=r'sections',)
+    def sections(self,request, pk = None):
+        sections = Section.objects.filter(board_id = pk)
+        serializer = SectionSerializer(sections, many = True)
         return Response(serializer.data)
 
 class SectionViewSet(viewsets.ModelViewSet):
@@ -70,11 +76,32 @@ class SectionViewSet(viewsets.ModelViewSet):
         serializer = SectionSerializer(sections, many = True)
         return Response(serializer.data)
     serializer_class = SectionSerializer
+    @action(detail=True, methods=["get"], url_path=r'tasks',)
+    def tasks(self,request, pk = None):
+        tasks = Task.objects.filter(section_id = pk)
+        serializer = TaskSerializer(tasks, many = True)
+        return Response(serializer.data)
 
 class GroupViewSet(viewsets.ModelViewSet):
     permission_classes = [AllowAny]
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+    @action(detail=True, methods=["get"], url_path=r'users',)
+    def profiles(self,request, pk = None):
+        profiles = Group.objects.get(pk=pk).profile_set.all()
+        serializer = ProfileSerializer(profiles, many = True)
+        return Response(serializer.data)
+    @action(detail=True, methods=["post"], url_path=r'add_users',)
+    def add_users(self,request, pk = None):
+        try:
+            body_data = json.loads(request.body.decode("utf-8"))
+        except Exception as e:
+            return HttpResponseBadRequest(json.dumps({'error': 'Invalid request: {0}'.format(str(e))}), content_type="application/json")
+        profiles = Profile.objects.filter(id__in = body_data["users"])
+        serializer = ProfileSerializer(profiles, many = True)
+        group = Group.objects.get(pk=pk)
+        group.add(profiles)
+        return Response(serializer.data)
 
 class TaskViewSet(viewsets.ModelViewSet):
     permission_classes = [AllowAny]
@@ -90,6 +117,16 @@ class TaskViewSet(viewsets.ModelViewSet):
         serializer = TaskSerializer(tasks, many = True)
         return Response(serializer.data)
     serializer_class = TaskSerializer
+    @action(detail=True, methods=["get"], url_path=r'subtasks',)
+    def subtasks(self,request, pk = None):
+        subtasks = Subtask.objects.filter(task_id = pk)
+        serializer = SubtaskSerializer(subtasks, many = True)
+        return Response(serializer.data)
+    @action(detail=True, methods=["get"], url_path=r'comments',)
+    def comments(self,request, pk = None):
+        comments = Comment.objects.filter(task_id = pk)
+        serializer = CommentSerializer(comments, many = True)
+        return Response(serializer.data)
 
 class SubtaskViewSet(viewsets.ModelViewSet):
     permission_classes = [AllowAny]
